01221188f5ea6c04906af891cc9f7c5098ead2a4
comp11 <comp11@mail.ru>
2023-11-24 14:30:02 +0300

    закончила 123

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
-with open('example_100kb.csv') as f:
+domen_count = {}
+
+with open('out.csv') as f:
     for line in f:
-        print(line)
-    print('lol kek')
+        line = line.split(',')
+        for i in line:
+            if i.count('@')==1:
+                for j in range(len(i)):
+                    if i[j]=='@':
+                        b = i[j+1::]
+                        break
+
+        if b in domen_count:
+            domen_count[b] += 1
+        else:
+            domen_count[b] = 1
+
+print(domen_count)
+domen_sorted = sorted(domen_count.items(), key=lambda item: item[1], reverse=True)
+file = open('file.csv', 'w+')
+a = 'domen, count' + '\n'
+file.write(a)
+for i in domen_sorted:
+    a = str(i[0]) + ', ' + str(i[1]) + '\n'
+    file.write(a)
+print(file)
